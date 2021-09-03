42fe1313b5ebf2639978184a88936d27e7fad807
Kevin Xu <fzxu94@gmail.com>
2021-09-03 08:08:04 -0700

    Add log softmax as one of cost functions

--- src/cost.py
+++ src/cost.py
@@ -14,3 +14,13 @@ class MSE(Cost):
     @classmethod
     def grad(cls, y_prime: np.ndarray, y: np.ndarray) -> np.ndarray:
         return 2 * (y_prime - y)
+
+class LogSoftmax(Cost):
+
+    @classmethod
+    def eval(cls, y_prime: np.ndarray, y: np.ndarray) -> float:
+        return -y[y_prime == 1][0] + np.log(np.exp(y).sum())
+
+    @classmethod
+    def grad(cls, y_prime: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return -1 + np.exp(y[y_prime == 1][0]) / np.exp(y).sum()
