0b10b69a93a35ae04d9e2d85bfc59f6fe1402444
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2021-09-10 22:46:31 +0800

    Transpose the layer dimentions to be more conventional

--- src/layer.py
+++ src/layer.py
@@ -12,28 +12,30 @@ class Layer:
 
     def connect(self, prev_layer: 'Layer'):
         self.prev, prev_layer.next = prev_layer, self
-        self._w = np.random.random((self.n, prev_layer.n)) / 5. - .1 # weights
-        self._b = np.ones((self.n, 1)) * 0.1 # bias
+        # weights initialized to -0.1 to 0.1
+        self._w = np.random.random((prev_layer.n, self.n)) / 5. - .1
+        # bias initialized to 0.1
+        self._b = np.ones(self.n) * 0.1 # bias
 
     def forward_pass(self):
-        # a_L = act(w_L \dot a_(L-1) + b_L)
+        # a_L = act(a_(L-1) \dot w_L + b_L)
         if self.prev:
             # neuron values before activation
-            self._z = np.matmul(self._w, self.prev.a) + self._b
+            self._z = np.matmul(self.prev.a, self._w) + self._b
             # neuron values
             self.a = self._act.eval(self._z)
         if self.next:
             self.next.forward_pass()
 
     def back_prop(self, a_grad: np.ndarray):
-        # a_grad.shape == (n, p)
+        # a_grad.shape == (p, n)
         if self.prev:
-            # (n, p) = (n, p) * (n, p)
+            # (p, n) = (p, n) * (p, n) broadcast
             b_grad = a_grad * self._act.grad(self._z) # * 1.
-            # (n, m) = einsum('ik,jk', (n, p), (m, p))
-            w_grad = np.einsum('ik,jk', b_grad, self.prev.a)
-            # (m, p) = einsum('ij,ik', (n, m), (n, p))
-            prev_a_grad = np.einsum('ij,ik', self._w, b_grad)
-            self._b -= self._rate * np.sum(b_grad, axis=1)[:, np.newaxis]
+            # (m, n) = einsum('ki,kj', (p, m), (p, n))
+            w_grad = np.einsum('ki,kj', self.prev.a, b_grad)
+            # (p, m) = einsum('ik,jk', (p, n), (m, n))
+            prev_a_grad = np.einsum('ik,jk', b_grad, self._w)
+            self._b -= self._rate * np.sum(b_grad, axis=0)
             self._w -= self._rate * w_grad
             self.prev.back_prop(prev_a_grad)
