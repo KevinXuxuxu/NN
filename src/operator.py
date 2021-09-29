4faf9999958a93486530e255e885ddaa809fc954
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2021-09-30 00:42:41 +0800

    Add a few changes and MatMul

--- src/operator.py
+++ src/operator.py
@@ -1,19 +1,25 @@
 import numpy as np
 
-from typing import List
+from typing import List, Tuple
+from src.utils import check_shape
 
 
 class Operator:
 
-    def __init__(self):
+    def __init__(self, ishape: List[int], oshape: List[int]):
+        self.ishape, self.oshape = tuple(ishape), tuple(oshape)
         self.preds, self.succs= [], []
         self.forward_pass_count, self.back_prod_count = 0, 0
 
     def _forward_pass(self):
-        raise NotImplemented('forward_pass')
+        # override in subclass with this as callback
+        for succ in self.succs:
+            succ.forward_pass()
 
-    def _back_prop(self) -> np.ndarray:
-        raise NotImplemented('back_prop')
+    def _back_prop(self):
+        # override in subclass with this as callback
+        for pred in self.preds:
+            pred.back_prop()
 
     def forward_pass(self):
         self.forward_pass_count += 1
@@ -26,3 +32,39 @@ class Operator:
         if self.back_prod_count == len(self.succs):
             self._back_prop()
             self.back_prod_count = 0
+
+    def register(self, pred: 'Operator'):
+        self.preds.append(pred)
+        pred.succs.append(self)
+
+
+class ParameterizedOperator(Operator):
+
+    rate = 0.001
+
+    def __init__(self, ishape: List[int], oshape: List[int], rate: float = None):
+        super().__init__(ishape, oshape)
+        self._rate = rate if rate is not None else self.__class__.rate
+
+
+class MatMul(ParameterizedOperator):
+    
+    def __init__(self, ishape: List[int], oshape: List[int]):
+        super().__init__(ishape, oshape)
+        # weights initialized to [-0.1, 0.1]
+        self._w = np.random.random((ishape[0], oshape[0])) / 5. - .1
+
+    def register(self, pred: 'Operator'):
+        check_shape(pred.oshape, self.ishape)
+        super().register(pred)
+
+    def _forward_pass(self):
+        self.output = np.matmul(self.preds[0].output, self._w)
+        super()._forward_pass()
+
+    def _back_prop(self):
+        dL_do = np.sum(np.array([succ.grad for succ in self.succs]))
+        dL_dw = np.einsum('ki,kj', self.preds[0].output, dL_do)
+        self._w -= self._rate * dL_dw
+        self.grad = np.einsum('ik,jk', dL_do, self._w)
+        super()._back_prop()
--- test/operator_test.py
+++ test/operator_test.py
@@ -6,18 +6,13 @@ from src.operator import Operator
 class Plus(Operator):
 
     def __init__(self, p: int):
-        super().__init__()
+        super().__init__([0], [0])
         self.p = p
         self.output = None
 
-    def register(self, succ: Operator):
-        self.succs.append(succ)
-        succ.preds.append(self)
-
     def _forward_pass(self):
         self.output = sum(pred.output for pred in self.preds) + self.p
-        for succ in self.succs:
-            succ.forward_pass()
+        super()._forward_pass()
 
 
 class OperatorTest(unittest.TestCase):
@@ -25,19 +20,19 @@ class OperatorTest(unittest.TestCase):
     def test_forward_pass(self):
         expected_result = [0, 1, 2, 5, 9, 14, 23, 31, 53]
         a = [Plus(i) for i in range(9)]
-        a[0].register(a[1])
-        a[0].register(a[2])
-        a[1].register(a[6])
-        a[1].register(a[7])
-        a[2].register(a[6])
-        a[2].register(a[3])
-        a[3].register(a[6])
-        a[3].register(a[4])
-        a[4].register(a[6])
-        a[4].register(a[5])
-        a[5].register(a[8])
-        a[6].register(a[7])
-        a[7].register(a[8])
+        a[1].register(a[0])
+        a[2].register(a[0])
+        a[6].register(a[1])
+        a[7].register(a[1])
+        a[6].register(a[2])
+        a[3].register(a[2])
+        a[6].register(a[3])
+        a[4].register(a[3])
+        a[6].register(a[4])
+        a[5].register(a[4])
+        a[8].register(a[5])
+        a[7].register(a[6])
+        a[8].register(a[7])
         a[0].output = 0
         for succ in a[0].succs:
             succ.forward_pass()
